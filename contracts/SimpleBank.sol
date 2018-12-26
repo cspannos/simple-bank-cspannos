aa37426d76f716f17e26a7b7feec15a917ca1dad
root <root@muesli.newint>
2018-12-26 01:10:45 +0000

    Update withrawl function

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -79,7 +79,7 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw.
            return the user's balance.*/
-        if (withdrawAmount <= balances[msg.sender]) {
+        if (balances[msg.sender]) >= withdrawAmount) {
             balances[msg.sender] -= withdrawAmount;
             msg.sender.transfer(withdrawAmount);
         }
